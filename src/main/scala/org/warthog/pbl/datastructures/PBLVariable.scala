15fb1203b2d5386710582aebdb013cd43eab7d1e
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-09-15 13:54:02 +0200

    adding the ID instance to PBLVariable

--- src/main/scala/org/warthog/pbl/datastructures/PBLVariable.scala
+++ src/main/scala/org/warthog/pbl/datastructures/PBLVariable.scala
@@ -3,11 +3,17 @@ package org.warthog.pbl.datastructures
 import org.warthog.generic.formulas.Variable
 import org.warthog.pbl.PBL
 
+import scalaz.Id
+
 /**
  * Pseudo-boolean Variable
- * @param name the name of the variable
+ * @param name the name of the variable.
+ *             Note the variable name has to be of the form: x[Integer]
  */
 class PBLVariable(val name: String) {
+  //ID = Integer of the variable name
+  val ID = name.drop(1).toInt
+
   override def toString = name
 
   def copy = this
