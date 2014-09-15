fcc1adafe1a71366ac0c845035903fe149414298
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-09-15 13:52:52 +0200

    refactoring the parser

--- src/main/scala/org/warthog/pbl/parsers/PBCompetitionReader.scala
+++ src/main/scala/org/warthog/pbl/parsers/PBCompetitionReader.scala
@@ -3,6 +3,7 @@ package org.warthog.pbl.parsers
 import org.warthog.pbl.datastructures._
 
 import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
 
 /**
  * Parser to read pseudo-boolean constraint instances
@@ -16,7 +17,7 @@ object PBCompetitionReader {
    * @param path path of the source
    * @return a pair containing a list of all constraints an the objective function
    */
-  def readCompetitionFormat (path: String): (List[Constraint], List[PBLTerm]) = {
+  def readCompetitionFormat(path: String): (List[Constraint], Option[List[PBLTerm]]) = {
     var preambleRead = false
     var numberOfConstraintsInPreamble = 0
     var numberOfVarsInPreamble = 0
@@ -25,11 +26,12 @@ object PBCompetitionReader {
     val lines = io.Source.fromFile(path).getLines()
     var lineNumber = 0
 
-    var constraints = List[Constraint]()
-    var objectiveFunction = List[PBLTerm]()
+    var constraints = ListBuffer[Constraint]()
+    var objectiveFunction: Option[List[PBLTerm]] = None
 
     while (lines.hasNext) {
       lineNumber += 1
+      println("Line: " + lineNumber)
       val line = lines.next().trim.replaceAll("\\s+", " ")
 
       if (!line.isEmpty) {
@@ -40,37 +42,42 @@ object PBCompetitionReader {
               numberOfVarsInPreamble = tokens(1).trim.toInt
               numberOfConstraintsInPreamble = tokens(2).trim.toInt
               preambleRead = true
-            } else if(preambleRead && line.contains("#variable="))
+            } else if (preambleRead && line.contains("#variable="))
               System.err.println("Line " + lineNumber + ": More than one preamble --> Use the first")
-          case 'm' => objectiveFunction = parseObjective(line)
-          case _ => parseConstraint(line) match{
-            case (c, None) => constraints :+= c
-            case (c1, Some(c2)) => constraints :+= c1; constraints :+= c2; numberOfEqualityConstraints +=1
-          }
+          case 'm' =>
+            if(line.endsWith(";")) objectiveFunction = Some(parseObjective(line))
+            else System.err.println("Line " + lineNumber + ": Line doesn't end with ';' --> Skip line")
+          case _ =>
+            if(line.endsWith(";")){
+              parseConstraint(line) match {
+                case (c, None) => constraints += c
+                case (c1, Some(c2)) => constraints += c1; constraints += c2; numberOfEqualityConstraints += 1
+              }
+            } else System.err.println("Line " + lineNumber + ": Line doesn't end with ';' --> Skip line")
         }
       }
     }
 
     val vars = this.variables.size
     if (preambleRead) {
-      if (numberOfConstraintsInPreamble+numberOfEqualityConstraints != constraints.size)
+      if (numberOfConstraintsInPreamble + numberOfEqualityConstraints != constraints.size)
         System.err.println("Number of Clauses in Preamble: " + numberOfConstraintsInPreamble + ", " + "Number of computed Clauses: " + constraints.size)
       if (numberOfVarsInPreamble != vars)
         System.err.println("Number of Vars in Preamble: " + numberOfVarsInPreamble + ", " + "Number of computed Vars: " + vars)
     }
-    (constraints, objectiveFunction)
+    (constraints.toList, objectiveFunction)
   }
 
 
   /**
-   *  Parses a string that represents a constraint and returns:
-   *  - case '>=' constraint: a tuple (the constraint, None)
-   *  - case '=' constraint: a tuple ('>=' constraint, Some('<=' constraint))
+   * Parses a string that represents a constraint and returns:
+   * - case '>=' constraint: a tuple (the constraint, None)
+   * - case '=' constraint: a tuple ('>=' constraint, Some('<=' constraint))
    * @param line string representation of a constraint
    * @return the generated constraint
    */
-  private def parseConstraint(line: String) : (Constraint, Option[Constraint]) ={
-   //check if the line contains an '>=' or '=' constraint
+  private def parseConstraint(line: String): (Constraint, Option[Constraint]) = {
+    //check if the line contains an '>=' or '=' constraint
     val equalOperator = !line.contains(">=")
     //split the line into left-hand and right-hand side
     val splittedLine = line.split(">=|=")
@@ -80,14 +87,14 @@ object PBCompetitionReader {
     val termRegex = "[-|+]?\\s*\\d+\\s*x\\d+".r
     //compute the terms of the '>=' constraint
     val terms = termRegex.findAllIn(lhs)
-    val termList = terms.foldLeft(List[PBLTerm]())(_ :+ string2Term(_))
+    val termList = terms.foldLeft(ListBuffer[PBLTerm]())(_ += string2Term(_)).toList
 
     //compute the '>=' constraint
-    var constraint: Constraint = new PBLConstraint(termList,BigInt(rhs))
-    if(constraint.asInstanceOf[PBLConstraint].isCardinalityConstraint())
+    var constraint: Constraint = new PBLConstraint(termList, BigInt(rhs))
+    if (constraint.asInstanceOf[PBLConstraint].isCardinalityConstraint())
       constraint = new PBLCardinalityConstraint(constraint.terms, constraint.degree)
     //compute the <= constraint if necessary
-    val option = if(equalOperator) {
+    val option = if (equalOperator) {
       val lessOrEqualConstraint = constraint.copy
       lessOrEqualConstraint * -1
       lessOrEqualConstraint.normalize()
@@ -95,7 +102,6 @@ object PBCompetitionReader {
     } else {
       None
     }
-
     (constraint, option)
   }
 
@@ -106,8 +112,8 @@ object PBCompetitionReader {
    */
   private def parseObjective(line: String) = {
     val termRegex = "[-|+]?\\s*\\d+\\s*x\\d+".r
-    val terms = termRegex.findAllIn(line.replace("min:",""))
-    terms.foldLeft(List[PBLTerm]())(_ :+ string2Term(_))
+    val terms = termRegex.findAllIn(line.replace("min:", ""))
+    terms.foldLeft(ListBuffer[PBLTerm]())(_ += string2Term(_)).toList
   }
 
   /**
@@ -119,41 +125,34 @@ object PBCompetitionReader {
     val splitted = term.trim.split("\\s+")
     var variableName = ""
     var variableID = 0
-    var termCoefficient : BigInt = 0
+    var termCoefficient: BigInt = 0
     //case term matches +|-\\s+x\\s+\\d+
-    if(splitted.size == 3) {
+    if (splitted.size == 3) {
       variableName = splitted(2).trim
       termCoefficient = BigInt(splitted(0) + splitted(1))
-     //case term = +|-x\\s+\\d+
+      //case term = +|-x\\s+\\d+
     } else {
       variableName = splitted(1)
       termCoefficient = BigInt(splitted(0))
     }
     variableID = variableName.drop(1).toInt
-    variables.get(variableID)  match {
-       case None =>
-         //compute new variable an add the variable to the HashMap
-          val vari: PBLVariable = new PBLVariable(variableName)
-          variables += variableID -> vari
-          new PBLTerm(termCoefficient, new PBLLiteral(vari))
-        case Some(v) => new PBLTerm(termCoefficient, new PBLLiteral(v))
-      }
+    variables.get(variableID) match {
+      case None =>
+        //compute new variable an add the variable to the HashMap
+        val vari: PBLVariable = new PBLVariable(variableName)
+        variables += variableID -> vari
+        new PBLTerm(termCoefficient, new PBLLiteral(vari))
+      case Some(v) => new PBLTerm(termCoefficient, new PBLLiteral(v))
+    }
   }
-
 }
 
 
 object Main {
   def main(args: Array[String]) {
-    val startTime = System.currentTimeMillis()
-    val pair = PBCompetitionReader.readCompetitionFormat("C:\\Users\\Patrick\\Desktop\\Masterarbeit\\PB competition instances\\PB11\\normalized-PB11\\DEC-SMALLINT-LIN\\heinz\\normalized-neos808444.opb")
-    val elapsedTime = System.currentTimeMillis()-startTime
-    println("Time in milli: " + elapsedTime)
-    println("Time in min: " + elapsedTime/60000)
-    println("objective function: " + pair._2)
-    println()
-    println("Constraints: ")
-    //pair._1.foreach(println(_))
+    val pair = PBCompetitionReader.readCompetitionFormat("C:\\Users\\Patrick\\Desktop\\Masterarbeit\\PB competition instances\\PB11\\normalized-PB11\\DEC-SMALLINT-LIN\\lopes\\normalized-144.opb")
+    println(pair._1.size)
+    //println("objective: " + pair._2)
 
   }
 
