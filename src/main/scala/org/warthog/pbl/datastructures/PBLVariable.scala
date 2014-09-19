886cfc9afa7dcf289898e5d2cbb4352b941b97c5
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-09-19 11:38:47 +0200

    adds an assign method to class PBLVariable

--- src/main/scala/org/warthog/pbl/datastructures/PBLVariable.scala
+++ src/main/scala/org/warthog/pbl/datastructures/PBLVariable.scala
@@ -11,9 +11,10 @@ import scala.collection.mutable.ListBuffer
 class PBLVariable(val name: String) {
   //ID = Integer of the variable name
   val ID = name.drop(1).toInt
+  var state: State.Value = State.OPEN
   var watched = ListBuffer[Constraint]()
   var level: Int = -1
-  var state: State.Value = State.OPEN
+  var reason: Constraint = null
 
   override def toString = name
 
@@ -22,6 +23,35 @@ class PBLVariable(val name: String) {
   def add(c: Constraint) = watched += c
 
   def remove(c: Constraint) = watched -= c
+
+  /**
+   * Assign the variable and return an empty constraint if one exists else None
+   * If an unit constraint was computed the unitConstraints-list is updated as side-effect
+   * @param value the assigned value of the variable
+   * @param unitConstraints a list of unit constraints which has to be updated
+   * @param level the level of the variable
+   * @param reason the reason why the variable is assigned
+   * @return Some(empty-clause) or None
+   */
+  def assign(value: Boolean, unitConstraints: ListBuffer[Constraint], level: Int, reason: Constraint): Option[Constraint] = {
+    var emptyConstraint: Option[Constraint] = None
+    //update state, level and reason
+    if(value)
+      state = State.TRUE
+    else
+      state = State.FALSE
+    this.level = level
+    this.reason = reason
+
+    //update the constraints in watched list
+    watched.map{c =>
+      c.updateWatchedLiterals(this,value) match {
+        case ConstraintState.SAT => unitConstraints += c
+        case ConstraintState.EMPTY => emptyConstraint = Some(c)
+      }
+    }
+    emptyConstraint
+  }
 }
 
 object State extends Enumeration {
