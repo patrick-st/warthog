9aeaf5f11ae1a2e415e9a539c1af9a08e25ad691
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-09-26 18:40:25 +0200

    uses the constraints of file C.scala

--- src/test/scala/org/warthog/pbl/parsers/PBCompetitionReaderTest.scala
+++ src/test/scala/org/warthog/pbl/parsers/PBCompetitionReaderTest.scala
@@ -3,10 +3,11 @@ package org.warthog.pbl.parsers
 import java.io.File
 
 import org.specs2.mutable.Specification
+import org.warthog.pbl.C
 import org.warthog.pbl.datastructures._
 
 /**
- * Created by Patrick on 25.09.2014.
+ * Tests for the PBCompetitionReader
  */
 class PBCompetitionReaderTest extends Specification{
 val hello = "hello"
@@ -16,77 +17,37 @@ val hello = "hello"
   val correctInstance = PBCompetitionReader.readCompetitionFormat(getFileString("correctFormat.opb"))
   val incorrectInstance = PBCompetitionReader.readCompetitionFormat(getFileString("incorrectFormat.opb"))
 
-  val x1 = new PBLVariable("x1")
-  val x2 = new PBLVariable("x2")
-  val x3 = new PBLVariable("x3")
-  val x4 = new PBLVariable("x4")
-  val x5 = new PBLVariable("x5")
 
-  val objectiveFunction = List[PBLTerm](new PBLTerm(1,new PBLLiteral(x1)),
-                                        new PBLTerm(1,new PBLLiteral(x2)),
-                                        new PBLTerm(1,new PBLLiteral(x3)),
-                                        new PBLTerm(1,new PBLLiteral(x4)),
-                                        new PBLTerm(1,new PBLLiteral(x5)))
 
-  val terms1 = List[PBLTerm](new PBLTerm(4, new PBLLiteral(x1)),
-                             new PBLTerm(3, new PBLLiteral(x2)),
-                             new PBLTerm(2, new PBLLiteral(x3)),
-                             new PBLTerm(1, new PBLLiteral(x4)))
 
-  val terms2 = List[PBLTerm](new PBLTerm(1, new PBLLiteral(x1)),
-                             new PBLTerm(1, new PBLLiteral(x2)),
-                             new PBLTerm(1, new PBLLiteral(x4)),
-                             new PBLTerm(1, new PBLLiteral(x5)))
 
-  val terms3 = List[PBLTerm](new PBLTerm(1, new PBLLiteral(x3)),
-                             new PBLTerm(1, new PBLLiteral(x4)),
-                             new PBLTerm(1, new PBLLiteral(x5)))
 
-  val terms4 = List[PBLTerm](new PBLTerm(-1, new PBLLiteral(x1)),
-                             new PBLTerm(-2, new PBLLiteral(x2)),
-                             new PBLTerm(-3, new PBLLiteral(x3)),
-                             new PBLTerm(-4, new PBLLiteral(x4)),
-                             new PBLTerm(-5, new PBLLiteral(x5)))
-
-  val terms5_1 = List[PBLTerm](new PBLTerm(-1, new PBLLiteral(x1)),
-                             new PBLTerm(1, new PBLLiteral(x2)),
-                             new PBLTerm(1, new PBLLiteral(x3)))
-
-  val terms5_2 = List[PBLTerm](new PBLTerm(1, new PBLLiteral(x1)),
-                               new PBLTerm(-1, new PBLLiteral(x2)),
-                               new PBLTerm(-1, new PBLLiteral(x3)))
-  val c1 = new PBLConstraint(terms1, 5)
-  val c2 = new PBLCardinalityConstraint(terms2, 2)
-  val c3 = new PBLCardinalityConstraint(terms3, 1)
-  val c4 = new PBLConstraint(terms4, -2)
-  val c5_1 = new PBLCardinalityConstraint(terms5_1, 2)
-  val c5_2 = new PBLCardinalityConstraint(terms5_2, -2)
 
 
   "correctFormat.opb" should {
     "contain objectiveFunction" in {
-      correctInstance._2.get must be equalTo objectiveFunction
+      correctInstance._2.get must be equalTo C.objectiveFunction
     }
     "have 6 constraints" in {
       correctInstance._1.size == 6 must be equalTo true
     }
     "contain c1" in {
-      correctInstance._1 must contain(c1)
+      correctInstance._1 must contain(C.c1)
     }
     "contain c2" in {
-      correctInstance._1 must contain(c2)
+      correctInstance._1 must contain(C.c2)
     }
     "contain c3" in {
-      correctInstance._1 must contain(c3)
+      correctInstance._1 must contain(C.c3)
     }
     "contain c4" in {
-      correctInstance._1 must contain(c4)
+      correctInstance._1 must contain(C.c4)
     }
     "contain c5_1" in {
-      correctInstance._1 must contain(c5_1)
+      correctInstance._1 must contain(C.c5_1)
     }
     "contain c5_2" in {
-      correctInstance._1 must contain(c5_2)
+      correctInstance._1 must contain(C.c5_2)
     }
   }
 
@@ -98,22 +59,22 @@ val hello = "hello"
       incorrectInstance._1.size == 4 must be equalTo true
     }
     "contain c1" in {
-      incorrectInstance._1 must contain(c1)
+      incorrectInstance._1 must contain(C.c1)
     }
     "contain c2" in {
-      incorrectInstance._1 must contain(c2)
+      incorrectInstance._1 must contain(C.c2)
     }
     "contain not c3" in {
-      incorrectInstance._1.contains(c3) must be equalTo false
+      incorrectInstance._1.contains(C.c3) must be equalTo false
     }
     "contain c4" in {
-      incorrectInstance._1.contains(c4) must be equalTo false
+      incorrectInstance._1.contains(C.c4) must be equalTo false
     }
     "contain c5_1" in {
-      incorrectInstance._1 must contain(c5_1)
+      incorrectInstance._1 must contain(C.c5_1)
     }
     "contain c5_2" in {
-      incorrectInstance._1 must contain(c5_2)
+      incorrectInstance._1 must contain(C.c5_2)
     }
   }
 
