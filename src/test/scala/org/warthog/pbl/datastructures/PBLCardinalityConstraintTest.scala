9ff46f14a05677a7c8dc65c22c48ea4a68b77a8f
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-09-30 19:34:58 +0200

    adds tests for PBLCardinalityConstraints

---
+++ src/test/scala/org/warthog/pbl/datastructures/PBLCardinalityConstraintTest.scala
@@ -0,0 +1,96 @@
+package org.warthog.pbl.datastructures
+
+import org.specs2.mutable.Specification
+import org.warthog.pbl.C
+
+import scala.collection.mutable.ListBuffer
+
+/**
+ * Tests for PBLCardinalityConstraints
+ */
+class PBLCardinalityConstraintTest extends Specification {
+  sequential
+
+  "c2" should {
+    "be equal to c2" in {
+      C.c2 must be equalTo C.c2
+    }
+    "be equal to c2_1" in {
+      C.c2 must be equalTo C.c2_1
+    }
+    "not be equal to c2_2" in {
+      C.c2 == C.c2_2 must be equalTo false
+    }
+    "not be equal to c2_3" in {
+      C.c2 == C.c1_3 must be equalTo false
+    }
+    "not be equal to c1" in {
+      C.c2 == C.c1 must be equalTo false
+    }
+  }
+
+  "After initializing the watched literals, c2" should {
+    "have the ConstraintState SUCCESS" in{
+      val state = C.c2.initWatchedLiterals()
+      state must be equalTo ConstraintState.SUCCESS
+    }
+    "watch literal l1" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l1)) must be equalTo true
+    }
+    "watch literal l2" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l2)) must be equalTo true
+    }
+    "watch literal l4" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l4)) must be equalTo true
+    }
+    "not watch literal l5" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l5)) must be equalTo false
+    }
+  }
+
+  val units = ListBuffer[Constraint]()
+  "After assigning x1 and x2 to false, c2" should {
+    "be unit" in {
+      C.x1.assign(false,units,1,null)
+      C.x2.assign(false, units,1, null)
+      units.contains(C.c2) must be equalTo true
+    }
+    "watch literal l2" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l2)) must be equalTo true
+    }
+    "watch literal l4" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l4)) must be equalTo true
+    }
+    "watch literal l5" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l5)) must be equalTo true
+    }
+    "not watch literal l1" in {
+      C.c2.watchedLiterals.contains( new PBLTerm(1, C.l1)) must be equalTo false
+    }
+  }
+
+  "After assigning x3 to false, c2" should {
+    "be empty" in {
+      C.x4.assign(false,units,1,null).get must be equalTo C.c2
+    }
+  }
+
+  "After initializing the watched literals, emptyCardinalityConstraint" should {
+    "be empty" in {
+      C.emptyCardinalityConstraint.initWatchedLiterals() must be equalTo ConstraintState.EMPTY
+    }
+  }
+
+  "After initializing the watched literals, unitCardinalityConstraint" should {
+    "be unit" in {
+      C.unitCardinalityConstraint.initWatchedLiterals() must be equalTo ConstraintState.UNIT
+    }
+  }
+
+  "After initializing the watched literals, unitCardinalityConstraint" should {
+    "be sat" in {
+      C.satCardinalityConstraint.initWatchedLiterals() must be equalTo ConstraintState.SAT
+    }
+  }
+
+}
