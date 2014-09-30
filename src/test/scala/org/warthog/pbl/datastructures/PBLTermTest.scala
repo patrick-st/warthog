19425e35cff3246992ea9cbd4086b43f2b4ae71a
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-09-30 15:55:12 +0200

    adds some tests for PBLTerms

---
+++ src/test/scala/org/warthog/pbl/datastructures/PBLTermTest.scala
@@ -0,0 +1,29 @@
+package org.warthog.pbl.datastructures
+
+import org.specs2.mutable.Specification
+import org.warthog.pbl.C
+
+/**
+ * Test for PBLTerm
+ */
+class PBLTermTest extends Specification{
+
+  "t1" should {
+    "be equal to t1" in {
+      C.t1 must be equalTo C.t1
+    }
+    "be equal to t1_1" in {
+      C.t1 must be equalTo C.t1_1
+    }
+    "not be equal to t1_2" in {
+      C.t1 == C.t1_2 must be equalTo false
+    }
+    "not be equal to t1_3" in {
+      C.t1 == C.t1_3 must be equalTo false
+    }
+    "not be equal to t2" in {
+      C.t1 == C.t2 must be equalTo false
+    }
+  }
+
+}
