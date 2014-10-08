bf56c433da540f9751f88b580feef139319b725c
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-10-08 15:59:43 +0200

    refactoring CDCLLike

--- src/main/scala/org/warthog/pbl/decisionprocedures/CDCLLike.scala
+++ src/main/scala/org/warthog/pbl/decisionprocedures/CDCLLike.scala
@@ -13,13 +13,13 @@ class CDCLLike {
   val objectiveFunction = List[PBLTerm]()
   var level = 0
   val stack = mutable.Stack[PBLVariable]()
-  var variables = mutable.HashMap[Int,PBLVariable]()
+  var variables = mutable.HashMap[Int, PBLVariable]()
   var units = ListBuffer[Constraint]()
-  var occurrences =  mutable.HashMap[PBLVariable, Int]()
+  var occurrences = mutable.HashMap[PBLVariable, Int]()
 
-  def this(instance: List[Constraint], variables: mutable.HashMap[Int, PBLVariable]){
+  def this(instance: List[Constraint], variables: mutable.HashMap[Int, PBLVariable]) {
     this()
-    this.instance = instance.map{c =>
+    this.instance = instance.map { c =>
       c.initWatchedLiterals match {
         case ConstraintState.UNIT => this.units :+= c
         case _ =>
@@ -30,36 +30,28 @@ class CDCLLike {
     this.occurrences = this.computeOccurrences(instance)
   }
 
-
-  private def computeOccurrences(instance: List[Constraint]): mutable.HashMap[PBLVariable, Int] = {
-    var occ = mutable.HashMap[PBLVariable, Int]()
-    instance.map(_.terms.map{t =>
-     occ.get(t.l.v) match {
-       case Some(o) => occ.update(t.l.v,o + 1)
-       case None => {
-         occ.+=((t.l.v, 1))
-       }
-     }
-    })
-    occ
-  }
-
-
-  def solve : Boolean = {
-    while(true){
+  /**
+   * Main entry point for solving the given instance
+   * @return true if the instance is sat else false
+   */
+  def solve: Boolean = {
+    while (true) {
       this.unitPropagation match {
         case Some(c) => {
+          //conflict occurred => conflict has to be analyzed
           val backtrackLevel = this.analyzeConflict(c)
-          if(backtrackLevel == -1)
+          if (backtrackLevel == -1)
             return false
+          //backtracking to the computed level
           this.backtrack(backtrackLevel)
         }
         case None => {
+          //non conflict occurred => assign a new chosen variable
           this.getUnassignedVar match {
             case None => return true
             case Some(v) => {
               this.level += 1
-              v.assign(false,units,level,null)
+              v.assign(false, units, level, null)
               this.stack.push(v)
             }
           }
@@ -69,23 +61,43 @@ class CDCLLike {
     false
   }
 
+  /**
+   * Method computes the occurrences of the variables of the given instance
+   * @param instance
+   * @return the occurrences of the variables
+   */
+  private def computeOccurrences(instance: List[Constraint]): mutable.HashMap[PBLVariable, Int] = {
+    var occ = mutable.HashMap[PBLVariable, Int]()
+    instance.map(_.terms.map { t =>
+      occ.get(t.l.v) match {
+        case Some(o) => occ.update(t.l.v, o + 1)
+        case None => {
+          occ.+=((t.l.v, 1))
+        }
+      }
+    })
+    occ
+  }
+
 
   /**
    * Treat all unit constraints and all literals which has to be propagated
    * @return
    */
-   def unitPropagation: Option[Constraint] ={
-    while(!units.isEmpty){
-      for(unit <- units){
+  private def unitPropagation: Option[Constraint] = {
+    while (!units.isEmpty) {
+      for (unit <- units) {
         val literals = unit.getLiteralsToPropagate
-        literals.map{l =>
+        //propagate all literals
+        literals.map { l =>
           val conflict: Option[Constraint] =
-            if(l.phase)
+            if (l.phase)
               l.v.assign(true, units, level, unit)
             else
               l.v.assign(false, units, level, unit)
           stack.push(l.v)
           conflict match {
+            //return the conflict if one occurs
             case Some(c) => {
               return Some(c)
             }
@@ -99,13 +111,17 @@ class CDCLLike {
     None
   }
 
-
-  def analyzeConflict(emptyClause: Constraint): Int = {
+  /**
+   * Analyze the conflict and compute the level to backtrack
+   * @param emptyClause the conflict
+   * @return the backtrack level
+   */
+ private def analyzeConflict(emptyClause: Constraint): Int = {
     var backtrackLevel = -1
-    if(this.level == 0)
+    if (this.level == 0)
       return -1
     //compute the clause to learn
-    var learnedClause = LearnUtil.learnClause(emptyClause,stack,level)
+    var learnedClause = LearnUtil.learnClause(emptyClause, stack, level)
     //compute backtracking level
     for (t <- learnedClause.terms) {
       val level: Int = t.l.v.level
@@ -113,29 +129,35 @@ class CDCLLike {
         backtrackLevel = level
       }
     }
-    learnedClause = this.setWatchedLiterals(learnedClause, this.level, backtrackLevel)
+    learnedClause = this.setWatchedLiterals(learnedClause, backtrackLevel)
+    //add the learned clause to the instance
     instance +:= learnedClause
     //update the units
     units = ListBuffer[Constraint](learnedClause)
-
     //if learnedClause has only one literal
-    if(learnedClause.terms.size == 1)
+    if (learnedClause.terms.size == 1)
       return 0
-
     backtrackLevel
   }
 
-
-  def backtrack(level: Int): Unit ={
-    while(!stack.isEmpty && stack.top.level > level){
+  /**
+   * Backtracking to the given level
+   * @param level to backtrack
+   */
+  private def backtrack(level: Int): Unit = {
+    while (!stack.isEmpty && stack.top.level > level) {
       val vari = stack.pop()
       vari.unassign()
     }
     //set the new level
     this.level = level
-    //update all PBLConstraints
-    instance.map{c =>
-      if(c.isInstanceOf[PBLConstraint]){
+    /* Update all PBLConstraints
+    * Not sure if this has to be done right here or could be done while computing the learned clause.
+    * The problem is if you want to learn PBLConstraints you need the slack under the current assignment.
+    * Updating the slack while computing the learned clause would lead to problems (in my opinion)
+    */
+    instance.map { c =>
+      if (c.isInstanceOf[PBLConstraint]) {
         //update slack and currentSum
         c.asInstanceOf[PBLConstraint].updateSlack
         c.asInstanceOf[PBLConstraint].updateCurrentSum
@@ -143,48 +165,61 @@ class CDCLLike {
     }
   }
 
-
+  /**
+   * Compute the next variable, which should be assigned
+   * The more a variable occurs the earlier it is chosen
+   * @return the next variable to assign
+   */
   private def getUnassignedVar: Option[PBLVariable] = {
-    var unassigned: PBLVariable = null
+    // get all open variables
     val openVars = variables.values.filter(_.state == State.OPEN).toList
-    if(openVars.isEmpty){
+    if (openVars.isEmpty) {
       None
     } else {
-     Some(openVars.maxBy(this.occurrences.get(_).get))
+      //find the variable with highest occurrence
+      Some(openVars.maxBy(this.occurrences.get(_).get))
     }
   }
-  
 
-  private def setWatchedLiterals(c: Constraint, level: Int, backtrackLevel: Int): Constraint = {
-    if(c.isInstanceOf[PBLCardinalityConstraint]) {
-      val cardinality = c.asInstanceOf[PBLCardinalityConstraint]
-      val watched = new ArrayBuffer[PBLTerm](math.min(cardinality.terms.size, (cardinality.degree.+(1).toInt)))
-      if (cardinality.terms.size == cardinality.degree) {
-        //all literals have to be watched
-        cardinality.terms.copyToBuffer(watched)
-        watched.map(_.l.v.add(cardinality))
-      } else {
-        //set the literals which will be forced after backtracking
-        for (t <- cardinality.terms) {
-          if (t.l.v.level == level) {
-            watched += t
-            t.l.v.add(cardinality)
+
+  /**
+   * Method computes the watched literals of the new learned clause.
+   * @param c the new learned clause
+   * @param backtrackLevel the backtrack level
+   * @return the constraint c after setting the watched literals
+   */
+  private def setWatchedLiterals(c: Constraint, backtrackLevel: Int): Constraint = {
+    c match {
+      case cardinality: PBLCardinalityConstraint => {
+        val watched = new ArrayBuffer[PBLTerm](math.min(cardinality.terms.size, (cardinality.degree.+(1).toInt)))
+        if (cardinality.terms.size == cardinality.degree) {
+          //all literals have to be watched
+          cardinality.terms.copyToBuffer(watched)
+          watched.map(_.l.v.add(cardinality))
+        } else {
+          //set the literals which will be forced after backtracking
+          for (t <- cardinality.terms) {
+            if (t.l.v.level == level) {
+              watched += t
+              t.l.v.add(cardinality)
+            }
+          }
+          //set the rest of the literals
+          for (t <- cardinality.terms) {
+            if (watched.size != cardinality.degree.+(1) && !watched.contains(t) && t.l.v.level == backtrackLevel) {
+              watched += t
+              t.l.v.add(cardinality)
+            }
           }
         }
-        //set the rest of the literals
-        for (t <- cardinality.terms) {
-          if(watched.size != cardinality.degree.+(1) && !watched.contains(t) && t.l.v.level == backtrackLevel){
-            watched += t
-            t.l.v.add(cardinality)
-          }
-        }
+        cardinality.watchedLiterals = watched
+        cardinality
       }
-      cardinality.watchedLiterals = watched
-      cardinality
-
-    } else
-      c
+      //TODO treat pseudo-boolean constraints
+      case _ => c
+    }
   }
+
 }
 
 
