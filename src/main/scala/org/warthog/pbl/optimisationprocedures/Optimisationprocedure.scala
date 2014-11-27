ffb2ef9a2807d5a5b6f244b30361b9ddb67dbf58
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-11-27 16:52:47 +0100

    adding an interface for Pseudo-Boolean Optimiser

---
+++ src/main/scala/org/warthog/pbl/optimisationprocedures/Optimisationprocedure.scala
@@ -0,0 +1,29 @@
+package org.warthog.pbl.optimisationprocedures
+
+import org.warthog.pbl.datastructures.{PBLTerm, Constraint}
+
+/**
+ * Interface for Pseudo-Boolean
+ */
+trait Optimisationprocedure {
+
+
+  /**
+   * Add a constraint which has to be solved
+   * @param c the constraint
+   */
+  def add(c: Constraint): Unit
+
+  /**
+   * Add a several constraints which have to be solved
+   * @param constraints the constraints
+   */
+  def add(constraints: List[Constraint]): Unit
+
+  /**
+   * Solve the added and additional given constraints
+   * @param objectiveFunction to minimize
+   * @return None if the instance is unsat else the optimum
+   */
+  def solve(objectiveFunction: List[PBLTerm]): Option[BigInt]
+}
