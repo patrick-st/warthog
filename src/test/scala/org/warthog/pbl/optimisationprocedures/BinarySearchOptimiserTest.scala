70fd303181c5f82c9542cef4350cc1e4ac3e9d01
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2014-12-09 14:03:38 +0100

    adapting the tests to the constructor

--- src/test/scala/org/warthog/pbl/optimisationprocedures/BinarySearchOptimiserTest.scala
+++ src/test/scala/org/warthog/pbl/optimisationprocedures/BinarySearchOptimiserTest.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import org.specs2.mutable.Specification
 import org.warthog.pbl.C
+import org.warthog.pbl.decisionprocedures.CDCLLike
 import org.warthog.pbl.parsers.PBCompetitionReader
 
 /**
@@ -19,55 +20,55 @@ class BinarySearchOptimiserTest extends Specification{
   "Optimisation by binary search  of simple cnf formulas" should {
     "be 0 for formula f01.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f01.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(0)
     }
     "be 0 for formula f02.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f02.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(0)
     }
     "be 1 for formula f03.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f03.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 1 for formula f04.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f04.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 1 for formula f05.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f05.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 1 for formula f06.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f06.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 2 for formula f07.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f07.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(2)
     }
     "be 5 for formula f08.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f08.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(5)
     }
     "be 2 for formula f08.cnf with new objective function" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f08.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get)
       binaryOptimiser.reset()
@@ -75,19 +76,19 @@ class BinarySearchOptimiserTest extends Specification{
     }
     "be 3 for formula f09.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f09.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(3)
     }
     "be 3 for formula f10.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f10.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(3)
     }
     "be 2 for formula f11.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f11.cnf.opb"))
-      val binaryOptimiser = new BinarySearchOptimiser()
+      val binaryOptimiser = new BinarySearchOptimiser(new CDCLLike)
       binaryOptimiser.add(instance._1)
       binaryOptimiser.solve(instance._2.get) == Some(2)
     }
@@ -96,29 +97,27 @@ class BinarySearchOptimiserTest extends Specification{
   "Optimisation by binary search of pseudo-boolean formulas" should {
     "be 34 for formula mps-v2" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2.opb"))
-      val optimiser = new BinarySearchOptimiser()
+      val optimiser = new BinarySearchOptimiser(new CDCLLike)
       optimiser.add(instance._1)
       optimiser.solve(instance._2.get) == Some(34)
     }
     "be 5 for formula v2-stein9" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-stein9.opb"))
-      val optimiser = new BinarySearchOptimiser()
+      val optimiser = new BinarySearchOptimiser(new CDCLLike)
       optimiser.add(instance._1)
       optimiser.solve(instance._2.get) == Some(5)
     }
     "be 9 for formula v2-stein15" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-stein15.opb"))
-      val optimiser = new BinarySearchOptimiser()
+      val optimiser = new BinarySearchOptimiser(new CDCLLike)
       optimiser.add(instance._1)
       optimiser.solve(instance._2.get) == Some(9)
     }
     "be 3089 for formula v2-20-10" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-20-10.opb"))
-      val optimiser = new BinarySearchOptimiser()
+      val optimiser = new BinarySearchOptimiser(new CDCLLike)
       optimiser.add(instance._1)
       optimiser.solve(instance._2.get) == Some(3089)
     }
   }
-
-
 }
--- src/test/scala/org/warthog/pbl/optimisationprocedures/LinearOptimiserTest.scala
+++ src/test/scala/org/warthog/pbl/optimisationprocedures/LinearOptimiserTest.scala
@@ -21,55 +21,55 @@ class LinearOptimiserTest extends Specification {
   "Optimisation by linear search  of simple cnf formulas" should {
     "be 0 for formula f01.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f01.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(0)
     }
     "be 0 for formula f02.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f02.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(0)
     }
     "be 1 for formula f03.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f03.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 1 for formula f04.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f04.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 1 for formula f05.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f05.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 1 for formula f06.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f06.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(1)
     }
     "be 2 for formula f07.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f07.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(2)
     }
     "be 5 for formula f08.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f08.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(5)
     }
     "be 2 for formula f08.cnf with new objective function" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f08.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get)
       linearOptimiser.reset()
@@ -77,19 +77,19 @@ class LinearOptimiserTest extends Specification {
     }
     "be 3 for formula f09.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f09.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(3)
     }
     "be 3 for formula f10.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f10.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(3)
     }
     "be 2 for formula f11.cnf" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("f11.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(2)
     }
@@ -98,28 +98,27 @@ class LinearOptimiserTest extends Specification {
   "Optimisation by linear search  of simple pseudo-boolean formulas" should {
     "be 34 for formula mps-v2" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(34)
     }
     "be 5 for formula v2-stein9" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-stein9.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(5)
     }
     "be 9 for formula v2-stein15" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-stein15.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(9)
     }
     "be 3089 for formula v2-20-10" in {
       val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-20-10.opb"))
-      val linearOptimiser = new LinearOptimiser()
+      val linearOptimiser = new LinearOptimiser(new CDCLLike)
       linearOptimiser.add(instance._1)
       linearOptimiser.solve(instance._2.get) == Some(3089)
     }
   }
-
 }
