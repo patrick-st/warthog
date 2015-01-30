42f4531d4281391b7279cdddbc7c7160baac44a6
P. Steinmetz <p.steinmetz@student.uni-tuebingen.de>
2015-01-30 14:43:59 +0100

    restructuring the tests

--- src/test/scala/org/warthog/pbl/decisionprocedures/CDCLLikeTest.scala
+++ src/test/scala/org/warthog/pbl/decisionprocedures/CDCLLikeTest.scala
@@ -15,131 +15,109 @@ class CDCLLikeTest extends Specification {
 
   args(sequential = true)
 
+  val fs = System.getProperty("file.separator")
+
   private def getDIMACSFileString(file: String) =
     List("src", "test", "resources", "dimacs", file).mkString(File.separator)
 
-  private def getPBFileString(file: String) =
-    List("src", "test", "resources", "pbl", "opb", file).mkString(File.separator)
+  private def getPBFileString(directory: String, file: String) =
+    List("src", "test", "resources", "pbl", directory, file).mkString(File.separator)
 
 
-  val solver = new CDCLLike(LearnMethod.ClauseLearning)
+  val CDCLLike_clauseLearning = new CDCLLike(LearnMethod.ClauseLearning)
+  val CDCLLike_cardinalityLearning = new CDCLLike(LearnMethod.CardinalityLearning)
+  val CDCLLike_pbcLearning = new CDCLLike(LearnMethod.PBConstraintLearning)
 
-  "Satisfiability of simple dimacs formulas" should {
-    "be true for formula oneClauseFormula" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("oneClauseFormula.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be true for formula oneVariableFormula" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("oneVariableFormula.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be true for formula f01" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f01.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be true for formula f02" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f02.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be false for formula f03" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f03.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f04" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f04.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f05" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f05.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f06" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f06.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f07" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f07.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f08" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f08.cnf"))
-      val solver = new CDCLLike(LearnMethod.ClauseLearning)
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f09" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f09.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f10" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f10.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula f11" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("f11.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
+  private def testFile(directory: String, file: String, solver: DecisionProcedure, expResult: Int) = {
+    val expText = if (expResult == Solver.SAT) "SAT" else "UNSAT"
+    "File " + file should {
+      "be " + expText in {
+        solver.reset()
+        val computedResult = if(file.endsWith(".cnf"))
+          solver.solve(DIMACSReader.dimacs2PBConstraints(getDIMACSFileString(file))._1)
+        else
+          solver.solve(PBCompetitionReader.getInstance(getPBFileString(directory, file))._1)
+        computedResult must be equalTo expResult
+      }
     }
   }
 
-  "Satisfiability of harder dimacs formulas" should {
-    "be true for formula uf150-010" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("uf150-010.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be true for formula uf150-027" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("uf150-027.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be false for formula uuf150-011" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("uuf150-011.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula uuf150-024" in {
-      val instance = DIMACSReader.dimacs2PBConstraints(getDIMACSFileString("uuf150-024.cnf"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-  }
+  //test clause learning
+  //sipmle cnf formulas
+  testFile("", "oneClauseFormula.cnf", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("", "oneVariableFormula.cnf", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("", "f01.cnf", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("", "f02.cnf", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("", "f03.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f04.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f05.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f06.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f07.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f08.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f09.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f10.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "f11.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  //harder cnf formulas
+  testFile("", "uf150-010.cnf", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("", "uf150-027.cnf", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("", "uuf150-011.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("", "uuf150-024.cnf", CDCLLike_clauseLearning, Solver.UNSAT)
+  //opb files
+  testFile("opb", "normalized-elf.rf6.ucl.opb", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("opb", "normalized-elf.rf7.ucl.opb", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("opb", "normalized-elf.rf8.ucl.opb", CDCLLike_clauseLearning, Solver.UNSAT)
+  testFile("opb", "normalized-mps-v2.opb", CDCLLike_clauseLearning, Solver.SAT)
+  testFile("opb", "normalized-mps-v2-stein27.opb", CDCLLike_clauseLearning, Solver.SAT)
 
-  "Satisfiability of pseudo-boolean formulas" should {
-    "be false for formula normalized-elf.rf6.ucl" in {
-      val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-elf.rf6.ucl.opb"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula normalized-elf.rf7.ucl" in {
-      val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-elf.rf7.ucl.opb"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be false for formula normalized-elf.rf8.ucl" in {
-      val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-elf.rf8.ucl.opb"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.UNSAT
-    }
-    "be true for formula normalized-mps-v2" in {
-      val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2.opb"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-    "be true for formula normalized-mps-v2-stein27" in {
-      val instance = PBCompetitionReader.getInstance(getPBFileString("normalized-mps-v2-stein27.opb"))
-      solver.reset()
-      solver.solve(instance._1) == Solver.SAT
-    }
-  }
+  //test cardinality learning
+  testFile("", "oneClauseFormula.cnf", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("", "oneVariableFormula.cnf", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("", "f01.cnf", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("", "f02.cnf", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("", "f03.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f04.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f05.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f06.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f07.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f08.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f09.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f10.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "f11.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  //harder cnf formulas
+  testFile("", "uf150-010.cnf", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("", "uf150-027.cnf", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("", "uuf150-011.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("", "uuf150-024.cnf", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  //opb files
+  testFile("opb", "normalized-elf.rf6.ucl.opb", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("opb", "normalized-elf.rf7.ucl.opb", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("opb", "normalized-elf.rf8.ucl.opb", CDCLLike_cardinalityLearning, Solver.UNSAT)
+  testFile("opb", "normalized-mps-v2.opb", CDCLLike_cardinalityLearning, Solver.SAT)
+  testFile("opb", "normalized-mps-v2-stein27.opb", CDCLLike_cardinalityLearning, Solver.SAT)
+
+  //test pbc learning
+  testFile("", "oneClauseFormula.cnf", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("", "oneVariableFormula.cnf", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("", "f01.cnf", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("", "f02.cnf", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("", "f03.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f04.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f05.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f06.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f07.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f08.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f09.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f10.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "f11.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  //harder cnf formulas
+  testFile("", "uf150-010.cnf", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("", "uf150-027.cnf", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("", "uuf150-011.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("", "uuf150-024.cnf", CDCLLike_pbcLearning, Solver.UNSAT)
+  //opb files
+  testFile("opb", "normalized-elf.rf6.ucl.opb", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("opb", "normalized-elf.rf7.ucl.opb", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("opb", "normalized-elf.rf8.ucl.opb", CDCLLike_pbcLearning, Solver.UNSAT)
+  testFile("opb", "normalized-mps-v2.opb", CDCLLike_pbcLearning, Solver.SAT)
+  testFile("opb", "normalized-mps-v2-stein27.opb", CDCLLike_pbcLearning, Solver.SAT)
 }
--- src/test/scala/org/warthog/pbl/optimisationprocedures/LinearOptimiserTest.scala
+++ src/test/scala/org/warthog/pbl/optimisationprocedures/LinearOptimiserTest.scala
@@ -20,106 +20,108 @@ class LinearOptimiserTest extends Specification {
   private def getPBFileString(file: String) =
     List("src", "test", "resources", "pbl", file).mkString(File.separator)
 
+  val optimiser = new LinearOptimiser(new CDCLLike(LearnMethod.PBConstraintLearning))
+
   "Optimisation by linear search  of simple cnf formulas" should {
     "be 0 for formula f01.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f01.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(0)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(0)
     }
     "be 0 for formula f02.cnf" in {
+      optimiser.reset
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f02.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(0)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(0)
     }
     "be 1 for formula f03.cnf" in {
+      optimiser.reset
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f03.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(1)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(1)
     }
     "be 1 for formula f04.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f04.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(1)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(1)
     }
     "be 1 for formula f05.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f05.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(1)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(1)
     }
     "be 1 for formula f06.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f06.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(1)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(1)
     }
     "be 2 for formula f07.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f07.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(2)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(2)
     }
     "be 5 for formula f08.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f08.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(5)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(5)
     }
     "be 2 for formula f08.cnf with new objective function" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f08.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get)
-      linearOptimiser.min(C.f08_objectiveFunction.map(_.copy)) == Some(2)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get)
+      optimiser.min(C.f08_objectiveFunction.map(_.copy)) == Some(2)
     }
     "be 3 for formula f09.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f09.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(3)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(3)
     }
     "be 3 for formula f10.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f10.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(3)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(3)
     }
     "be 2 for formula f11.cnf" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("cnf" + fs + "f11.cnf.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(2)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(2)
     }
   }
 
-  "Optimisation by linear search  of simple pseudo-boolean formulas" should {
+  "Optimisation by linear search of pseudo-boolean formulas" should {
     "be 34 for formula mps-v2" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("opb" + fs + "normalized-mps-v2.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(34)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(34)
     }
     "be 5 for formula v2-stein9" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("opb" + fs + "normalized-mps-v2-stein9.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(5)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(5)
     }
     "be 9 for formula v2-stein15" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("opb" + fs + "normalized-mps-v2-stein15.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(9)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(9)
     }
     "be 3089 for formula v2-20-10" in {
+      optimiser.reset()
       val instance = PBCompetitionReader.getInstance(getPBFileString("opb" + fs + "normalized-mps-v2-20-10.opb"))
-      val linearOptimiser = new LinearOptimiser(new CDCLLike(LearnMethod.ClauseLearning))
-      linearOptimiser.add(instance._1)
-      linearOptimiser.min(instance._2.get) == Some(3089)
+      optimiser.add(instance._1)
+      optimiser.min(instance._2.get) == Some(3089)
     }
   }
 }
